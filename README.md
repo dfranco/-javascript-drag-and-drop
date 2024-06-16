966daf575b0c3408fe259eea122a8bcc8514e1d3
Davide Franco <482177+dfranco@users.noreply.github.com>
2024-06-16 05:57:19 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Javascript - Drag and drop
+
+This purpose of this repository is to learn and experiment how to drag and drop elements using vanilla Javascript.
+
+## Project structure
+
+```shell
+├── README.md
+├── app.js
+├── images
+│   └── king.png
+├── index.html
+└── style.css
+```
+
+## How to run ?
+
+Clone this repo and point your web browser to the [index.html](index.html) file. 
+
+## References
+
+MDN - Javascript reference
+
+[HTML Drag and Drop API](https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API)
+
+Inspired by tutorial: [Master JavaScript Drag and Drop with Chess Example!](https://youtu.be/EFJg4nUg9sM?si=N8KSzJcvmzR5co74)
+
+## Credits
+
+**Source code**
+
+Credits goes to [@kubowania](https://github.com/kubowania) (based on above tutorial)
+
+**Medias**
+
+[king.png taken from https://www.downloadclipart.net/large/9763-chess-piece-design.png](https://www.downloadclipart.net/large/9763-chess-piece-design.png)
---
+++ app.js
@@ -0,0 +1,51 @@
+const king = document.querySelector(".chess-piece");
+const squares = document.querySelectorAll(".square");
+const infoDisplay = document.querySelector("#info");
+
+king.addEventListener("drag", dragging);
+king.addEventListener("dragstart", dragStart);
+
+function dragEnter(e) {
+    e.target.classList.add("highlight");
+    infoDisplay.textContent = "You are entering the space of " + e.target.classList;
+}
+
+function DragLeave(e) {
+    infoDisplay.textContent = "You are leaving the space of " + e.target.classList;
+    e.target.classList.remove("highlight");
+}
+
+function dragDrop(e) {
+    e.target.append(beingDragged);
+    e.target.classList.remove("highlight");
+    infoDisplay.textContent = "You have dropped something into " + e.target.classList;
+}
+
+function dragEnd(e) {
+    e.target.classList.add("target");
+    setTimeout(() => e.target.classList.remove("target"), 100);
+    infoDisplay.textContent = "Your drag has ended in " + e.target.classList;
+}
+
+function dragOver (e) {
+    e.preventDefault();
+    infoDisplay.textContent = "You are dragging something over " + e.target.classList
+}
+
+squares.forEach(square => {
+    square.addEventListener("dragover", dragOver)
+    square.addEventListener("dragenter", dragEnter)
+    square.addEventListener("dragleave", DragLeave)
+    square.addEventListener("drop", dragDrop)
+    square.addEventListener("dragend", dragEnd)
+})
+
+let beingDragged;
+
+function dragStart (e) {
+    beingDragged = e.target;
+    infoDisplay.textContent = "dragging has started on " + beingDragged.id;
+}
+function dragging () {
+    infoDisplay.textContent = "You are dragging a " + beingDragged.id
+}
